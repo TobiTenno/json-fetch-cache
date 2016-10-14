99899fd5396e7a84761483de38e5e60ab8c94944
Matej Voboril <matej@voboril.org>
2016-10-14 00:26:05 -0500

    finishing promise rewrite, adding better example

--- jsoncache.js
+++ jsoncache.js
@@ -1,72 +1,111 @@
 'use strict';
-var request = require('request');
+const request = require('request-promise');
 
+/**
+ * Represents a JSON Cache
+ */
 class JSONCache {
+  
+  /**
+   * @param {string} _url URL for the location of the remote json
+   * @param {number} maximumCacheTime The maximum amount of time that the cache should be allowed to be stale
+   */
   constructor(url, maximumCacheTime) {
-    this.url = url;
-    this.maximumCacheTime = maximumCacheTime;
-    this.cache = null;
-    this.lastRefresh = null;
-    this.refreshing = false;
-    this.refreshQueue = [];
-  }
-  dataIsCurrent() {
-    return this.cache && Date.now() - this.cache.creation < this.maximumCacheTime
+    this._url              = url;
+    this._maximumCacheTime = maximumCacheTime || 3000;
+    this._cache            = null;
+    this._lastRefresh      = null;
+    this._refreshing       = false;
+    this._refreshQueue     = [];
   }
   
-  getData(callback) {
-    if (this.dataIsCurrent()) {
-      callback(null, this.cache);
-    }
-    else {
-      this.refresh(callback);
-    }
+  /**
+   * Whether or not the data has expired past the maximumCacheTime
+   * @readonly
+   * @type {boolean}
+   */
+  get dataIsCurrent(){
+    return this._cache && ((Date.now() - this._cache.creation) < this._maximumCacheTime);
   }
-  refresh(callback) {
-    var self = this;
-    this.refreshQueue.push(callback);
-    if (!this.refreshing) {
-      this.refreshing = true;
-      this.retrieve(function (err, data) {
-        if (!err) {
-          self.cache = data;
-          self.lastRefresh = Date.now();
-        }
-        self.refreshing = false;
-        self.processRefreshQueue(err, data);
-      });
-    }
-  }
-  retrieve(callback) {
-    var self = this;
-    request.get(this.url, function (err, response, body) {
-      if (err) {
-        return callback(err);
+  
+  /**
+   * Refresh the cached data
+   * @returns {Promise<string>} describing the status
+   * @private 
+   */
+  refresh(funct) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      self._refreshQueue.push(funct);
+      if (!self._refreshing) {
+        self._refreshing = true;
+        self.retrieve()
+          .then((data) => {
+            self._cache = data;
+            self._lastRefresh = Date.now();
+            self._refreshing = false;
+            self.processRefreshQueue(data);
+            resolve(data);
+          })
+          .catch(console.error);
       }
-      if (response.statusCode !== 200) {
-        var error
-        error = new Error(`${this.url} returned HTTP status ${response.statusCode}`)
-        return callback(error);
-      }
-      var data
-      try {
-        data = JSON.parse(body);
-      }
-      catch (e) {
-        data = null;
-      }
-      if (!data) {
-        var error
-        error = new Error(`Invalid JSON from ${this.url}`);
-        return callback(error);
-      }
-      callback(null, new WorldState(data, self.platform));
     });
   }
-  processRefreshQueue (err, data) {
-    while (this.refreshQueue.length) {
-      this.refreshQueue.shift()(err, data);
-    }
+
+  /**
+   * Retrieve the most recent data from the specified url
+   * 
+   */
+  retrieve() {
+    return new Promise((resolve, reject) => {
+      var self = this;
+      request(self._url)
+        .then(function (body) {
+          var data;
+          try {
+            data = JSON.parse(body);
+          }
+          catch (e) {
+            data = null;
+            reject(e);
+          }
+          if (!data) {
+            var error;
+            error = new Error(`Invalid JSON from ${self._url}`);
+            reject (error);
+          }
+          resolve(data);
+        })
+        .catch((error) => {
+            console.error(error);
+        }); 
+    });
+  }
+
+  processRefreshQueue (data) {
+    return new Promise((resolve, reject) => {
+      while (this._refreshQueue.length) {
+        this._refreshQueue.shift()()
+          .then((data) => resolve(data))
+          .catch(console.error);
+      }
+    });
+  }
+  
+  getData() {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      if (self.dataIsCurrent !== null && self.dataIsCurrent) {
+        console.log(self.dataIsCurrent);
+        resolve(self._cache);
+      } else {
+        self.refresh()
+          .then((data) => {
+            resolve(data); 
+            console.log(data)})
+          .catch(console.err);
+      }
+    });
   }
 }
 module.exports = JSONCache;
