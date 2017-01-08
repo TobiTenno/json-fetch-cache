7fb90717aca4d6c63c1c23a47f69e9786ffc50e2
Matej Voboril <matej@voboril.org>
2017-01-08 11:37:43 -0600

    better caching

--- jsoncache.js
+++ jsoncache.js
@@ -1,161 +1,60 @@
 'use strict';
-const request = require('request-promise');
 
-/**
- * Represents a JSON Cache
- */
+const http = require('http');
+const https = require('https');
+
 class JSONCache {
-  
-  /**
-   * Represents a JSONCache
-   * @constructor
-   * @param {string} url URL for the location of the remote json
-   * @param {number} maximumCacheTime The maximum amount of time that the cache should be allowed to be stale
-   */
-  constructor(url, maximumCacheTime) {
-    
-    /**
-     * URL for the cache to fetch from
-     * @type {string}
-     * @private 
-     */
-    this._url              = url;
-    
-    /**
-     * Maximum amount of time to cache data
-     * @type {number}
-     * @private 
-     */
-    this._maximumCacheTime = maximumCacheTime || 3000;
-    
-    /**
-     * The data cache, a JSON formatted object
-     * @type {json}
-     * @private 
-     */
-    this._cache            = null;
-    
-    /**
-     * Date corresponding to the last time the cache was refreshed
-     * @type {Date}
-     * @private 
-     */
-    this._lastRefresh      = null;
-    
-    /**
-     * Whether or not the cache is currently refreshing, 
-     *  determines whether or not the cache will be returned or delayed until refreshed
-     * @type {boolean}
-     * @private 
-     */
-    this._refreshing       = false;
-    
-    /**
-     * An array of refresh functions to be performed
-     * @type {Array<function>}
-     * @private 
-     */
-    this._refreshQueue     = [];
-  }
-  
-  /**
-   * Whether or not the data has expired past the maximumCacheTime
-   * @readonly
-   * @type {boolean}
-   * @private
-   */
-  get dataIsCurrent(){
-    return this._cache && ((Date.now() - this._cache.creation) < this._maximumCacheTime);
-  }
-  
-  /**
-   * Refresh the cached data
-   * @param {function} Function to push onto the refresh queue if there is currently a refresh in progress
-   * @returns {Promise<string>} describing the status
-   * @private 
-   */
-  refresh(funct) {
-    const self = this;
-    return new Promise((resolve, reject) => {
-      self._refreshQueue.push(funct);
-      if (!self._refreshing) {
-        self._refreshing = true;
-        self.retrieve()
-          .then((data) => {
-            self._cache = data;
-            self._lastRefresh = Date.now();
-            self._refreshing = false;
-            self.processRefreshQueue(data);
-            resolve(data);
-          })
-          .catch(console.error);
-      }
-    });
+  constructor(url, timeout) {
+    this.url = url;
+    this.protocol = this.url.startsWith('https') ? https : http;
+
+    this.timeout = timeout;
+    this.currentData = null;
+    this.lastUpdated = null;
+    this.updating = null;
+
+    this.updateInterval = setInterval(() => this.update(), this.timeout);
+    this.update();
   }
 
-  /**
-   * Retrieve the most recent data from the specified url
-   * @returns {Promise<json>}
-   * @private
-   */
-  retrieve() {
-    return new Promise((resolve, reject) => {
-      var self = this;
-      request(self._url)
-        .then(function (body) {
-          var data;
-          try {
-            data = JSON.parse(body);
-          }
-          catch (e) {
-            data = null;
-            reject(e);
-          }
-          if (!data) {
-            var error;
-            error = new Error(`Invalid JSON from ${self._url}`);
-            reject (error);
-          }
-          resolve(data);
-        })
-        .catch((error) => {
-            console.error(error);
-        }); 
-    });
-  }
-
-  /**
-   * Process the next entry in the event queue
-   * @returns {Promise<json>}
-   * @private
-   */
-  processRefreshQueue (data) {
-    return new Promise((resolve, reject) => {
-      while (this._refreshQueue.length) {
-        this._refreshQueue.shift()()
-          .then((data) => resolve(data))
-          .catch(console.error);
-      }
-    });
-  }
-  
-  /**
-   * Get the cached data
-   * @returns {Promise<json>}
-   */ 
   getData() {
-    const self = this;
+    if (this.updating) {
+      return this.updating;
+    }
+    return Promise.resolve(this.currentData);
+  }
+
+  getDataJson() {
+     if (this.updating) {
+       return this.updating.then(data => JSON.parse(data));
+     }
+     return Promise.resolve(JSON.parse(this.currentData));
+   }
+
+  update() {
+    this.updating = this.httpGet().then((data) => {
+      this.lastUpdated = Date.now();
+      this.currentData = data;
+      this.updating = null;
+      return this.currentData;
+    }).catch((err) => {
+      this.updating = null;
+      throw err;
+    });
+  }
+
+  httpGet() {
     return new Promise((resolve, reject) => {
-      if (self.dataIsCurrent !== null && self.dataIsCurrent) {
-        resolve(self._cache);
-      } else {
-        self.refresh()
-          .then((data) => {
-            resolve(data);
-          })
-          .catch(console.err);
-      }
+      const request = protocol.get(this.url, (response) => {
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          reject(new Error(`Failed to load page, status code: ${response.statusCode}`));
+        }
+        const body = [];
+        response.on('data', chunk => body.push(chunk));
+        response.on('end', () => resolve(body.join('')));
+      });
+      request.on('error', err => reject(err));
     });
   }
 }
-module.exports = JSONCache;
+module.export = JSONCache;
