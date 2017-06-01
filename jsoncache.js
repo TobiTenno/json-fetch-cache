d8f8cc01e99d026f1a1fc3fb6b274c1e033720a7
Kilian <Kaptard@users.noreply.github.com>
2017-06-01 14:07:48 +0200

    Add Retry on 5xx Server Responses
    Retry `httpGet()` on 5xx response with a 1s delay.
    `maxRetry` param added. Defaults to 30 (Stack may overflow on too high retry count, since each retry attempt won't resolve until one of the last one does)

--- jsoncache.js
+++ jsoncache.js
@@ -4,11 +4,12 @@ const http = require('http');
 const https = require('https');
 
 class JSONCache {
-  constructor(url, timeout, promiseLib = Promise) {
+  constructor(url, timeout, promiseLib = Promise, maxRetry) {
     this.url = url;
     this.protocol = this.url.startsWith('https') ? https : http;
 
     this.timeout = timeout;
+    this.retryCount = maxRetry || 30;
     this.currentData = null;
     this.lastUpdated = null;
     this.updating = null;
@@ -45,7 +46,12 @@ class JSONCache {
     return new this.Promise((resolve, reject) => {
       const request = this.protocol.get(this.url, (response) => {
         if (response.statusCode < 200 || response.statusCode > 299) {
-          reject(new Error(`Failed to load page, status code: ${response.statusCode}`));
+          if (response.statusCode > 499 && this.retryCount < 30) {
+            this.retryCount++;
+            setTimeout(() => this.httpGet().then(resolve), 1000);
+          } else {
+            reject(new Error(`Failed to load page, status code: ${response.statusCode}`));
+          }
         }
         const body = [];
         response.on('data', chunk => body.push(chunk));
